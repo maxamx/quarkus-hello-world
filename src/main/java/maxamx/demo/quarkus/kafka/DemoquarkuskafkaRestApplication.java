f17e34710cdbf9197617bff190ffc273cd4cc62f
maxamx <maxamx@gmail.com>
2021-09-23 15:28:56 -0300

    inicio

---
+++ src/main/java/maxamx/demo/quarkus/kafka/DemoquarkuskafkaRestApplication.java
@@ -0,0 +1,11 @@
+package maxamx.demo.quarkus.kafka;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class DemoquarkuskafkaRestApplication extends Application {
+}
